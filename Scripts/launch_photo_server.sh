2c2eccb0caaa8262fd932bb4617cf509460234be
Quentin Douasbin <quentin.douasbin@gmail.com>
2020-07-04 10:09:36 -0700

    handle videos in the automatic crontab script

--- Scripts/launch_photo_server.sh
+++ Scripts/launch_photo_server.sh
@@ -2,6 +2,11 @@
 
 cd /External/PhotosBambinage
 cd Scripts
+echo " > Organize photos"
 /usr/bin/python3.7 organize_pics.py >> log_organize_pics.txt &
+echo " > Organize videos"
+/usr/bin/python3.7 organize_videos.py >> log_organize_videos.txt &
+echo " > Create small photos"
 /usr/bin/python3.7 create_small.py  >> log_create_small.txt & 
+echo " > Done"
 cd $HOME
